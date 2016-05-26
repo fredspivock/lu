8af16de8bf7fda04988db6a3e441e5cee13a76ab
Fred Spivock <fredspivock@gmail.com>
2016-05-26 00:23:36 -0400

    'irst commit

---
+++ src/client/app/layout/ht-top-nav.directive.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.layout')
+    .directive('htTopNav', htTopNav);
+
+  /* @ngInject */
+  function htTopNav() {
+    var directive = {
+      bindToController: true,
+      controller: TopNavController,
+      controllerAs: 'vm',
+      restrict: 'EA',
+      link: link,
+      scope: {
+        'navline': '='
+      },
+      templateUrl: 'app/layout/ht-top-nav.html'
+    };
+
+    TopNavController.$inject = ['$scope'];
+
+    /* @ngInject */
+    function TopNavController($scope) {
+      var vm = this;
+      $scope.isCollapsed = true;
+    }
+
+    function link(scope, element, attrs) {
+      $(document).scroll(function() {
+        
+        if($(document).scrollTop() > 200) {
+          element.fadeOut();
+        }
+        else {
+           element.fadeIn();
+        }
+      });
+    }
+
+    return directive;
+  }
+})();
